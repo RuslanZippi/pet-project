faff387b6b9d895f753aba3c25df3535048345d7
Ruslan <ruslanpodkuiko@mail.ru>
2020-10-28 23:28:46 +0300

    update

--- Calculator/TestWork/src/main/java/main/controllers/CalculatorController.java
+++ Calculator/TestWork/src/main/java/main/controllers/CalculatorController.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
 @RestController
-public class GreetingController {
+public class CalculatorController {
 
 
     @Autowired
